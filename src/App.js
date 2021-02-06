76f2864f6151264a072331423331c34be7250df7
cyberpunked69 <67334755+cyberpunked69@users.noreply.github.com>
2021-02-06 17:14:31 +0530

    new data added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import India from "./components/India";
+import World from "./components/World";
+import Header from "./components/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  render(){
+    return(
+      <div className="container-fluid">
+       <Router>
+         <Header/>
+         <Switch>
+           <Route exact path = "/">
+             <India/>
+           </Route>
+           <Route exact path = "/india">
+             <India/>
+           </Route>
+           <Route exact path = "/world">
+             <World/>
+           </Route>
+         </Switch>
+
+       </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Statedata.js
@@ -0,0 +1,96 @@
+import React, {Component} from 'react';
+import {Accordion,Card,Button} from 'react-bootstrap';
+import Axios from 'axios';
+
+class StateData extends Component{
+    constructor(){
+        super()
+        this.state={
+            stateData : {}
+        }
+    }
+    componentDidMount(){
+        Axios.get("https://api.covid19india.org/state_district_wise.json").then(response=>{
+            this.setState({stateData:response.data})
+        })
+    }
+    render(){
+        
+        let keys = Object.keys(this.state.stateData)
+        return(
+            <div className="row">
+                <div className="col-md-12">
+                <Accordion>
+                    {keys.map((key,k)=>{
+                        console.log(k , key)
+                        if(k === 0){
+                            return("")
+                        }
+                        let districts = this.state.stateData[key].districtData
+                        let district_Keys = Object.keys(districts);
+
+                        let total_active = 0;
+                        let total_confirmed = 0;
+                        let total_deaths = 0;
+                        let total_recover = 0;
+
+                        let district_list = []
+                        for(let x in districts){
+                            total_active += districts[x].active
+                            total_confirmed += districts[x].confirmed
+                            total_deaths += districts[x].deceased
+                            total_recover += districts[x].recovered
+                            let ob = districts[x]
+                            ob["district_name"] = x
+                            district_list.push(ob)
+                        }
+                        //console.log(district_list)
+                        return(
+                            <Card>
+                            <Card.Header>
+                            <Accordion.Toggle as={Button} variant="primary" eventKey={k}>
+                                {key}  <span className="btn-dark p-1 mr-2">Total Cases - {total_confirmed} </span><span className="btn-dark p-1 mr-2"> Active - {total_active}</span><span className="btn-dark p-1 mr-2">Recovered - {total_recover} </span> <span className="btn-dark p-1 mr-2">Death - {total_deaths}</span>
+                            </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey={k}>
+                            <Card.Body>
+                                <table className="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <td>District</td>
+                                            <td>Confirmed</td>
+                                            <td>Active</td>
+                                            <td>Recovered</td>
+                                            <td>Deaths</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            district_list.map((itm,ky)=>{
+                                                return(
+                                                    <tr>
+                                                        <td>{itm.district_name}</td>
+                                                        <td>{itm.confirmed}</td>
+                                                        <td>{itm.active}</td>
+                                                        <td>{itm.recovered}</td>
+                                                        <td>{itm.deceased}</td>
+                                                    </tr>
+                                                )
+                                            })
+                                        }
+                                    </tbody>
+                                </table>
+                            </Card.Body>
+                            </Accordion.Collapse>
+                            </Card>
+                            )
+                    })}
+                    
+                </Accordion>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default StateData;
